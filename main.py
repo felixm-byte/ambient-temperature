a3f8180111eda60960a87f1bb9b9fc0c84bd100f
felixexists <72092800+felixm-byte@users.noreply.github.com>
2024-08-31 22:50:24 +0100

    Final design! 😎

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from machine import I2C, Pin, SPI
 from time import sleep
 from pico_i2c_lcd import I2cLcd
 from dht import DHT22
- 
+import random 
 import max7219
-
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 I2C_ADDR = i2c.scan()[0]
 lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
@@ -18,31 +17,21 @@ green = machine.Pin(27, machine.Pin.OUT)
 spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
 ss = Pin(5, Pin.OUT)
 
-display = max7219.Matrix8x8(spi, ss, 4)
-
-display.brightness(10)
-display.fill(0)
-scrolling_message = "Happy day!"
-#Get the message length
-length = len(scrolling_message)
-
-#Calculate number of columns of the message
-column = (length * 8)
-
-#Clear the display.
-display.fill(0)
-display.show()
 
 def set_color(r,g,b):
     red.value(r)
     green.value(g)
     blue.value(b)
 
-
+options = ["^_^ - I'm finally awake", "Hi!, have a nice day", "Good day, mate!", "Have a great day!", "Have a good one."]
+index = 0
 while True:
     dht.measure()
     t = dht.temperature()
     h = dht.humidity()
+    lcd.putstr(options[index])
+    sleep(5)
+    lcd.clear()
     lcd.putstr("{}oC and {}% humidity".format(t, h))
     sleep(5)        # "Hello world!" text would be displayed for 5 secs
     lcd.clear()
@@ -57,16 +46,9 @@ while True:
         lcd.putstr("Sounds like a nice temperature,")
         set_color(0,1,0)
     sleep(2)
-    lcd.clear()  
-    for x in range(32, -column, -1):     
-    #Clear the display
-        display.fill(0)
-
-        # Write the scrolling text in to frame buffer
-        display.text(scrolling_message ,x,0,1)
-        
-        #Show the display
-        display.show()
-      
-        #Set the Scrolling speed. Here it is 50mS.
-        time.sleep(0.05)
+    lcd.clear()
+    if (index != len(options) - 1):
+        index += 1
+    else:
+        index = 0
+    
